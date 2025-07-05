c105c774a7915edb7ef6ef7b8e1afb139b4a2bde
Navodit <navoditsharma09@gmail.com>
2025-07-05 15:16:36 +0530

    added a css files and fixed a few things by importing and defining the path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProductDetailPage from './components/ProductDetailPage';
+
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
