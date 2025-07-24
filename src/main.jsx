ba4498a004b079fb9997a365f5dd6b13fe7bdc0a
Navodit <navoditsharma09@gmail.com>
2025-07-24 15:32:52 +0530

    Created a CartContext page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { CartProvider } from './context/CartContext'; // 👈 import
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CartProvider> {/* 👈 Wrap your App here */}
+      <App />
+    </CartProvider>
+  </StrictMode>
+);
